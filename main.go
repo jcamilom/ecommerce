b0fe682ed957f41e9b412f7d14e0701929b5690f
Camilo Muñoz <jcamilomunozz@gmail.com>
2019-10-05 14:52:44 -0500

    Added createUser handler

--- main.go
+++ main.go
@@ -16,6 +16,29 @@ var (
 	port = 3000
 )
 
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	ur := new(createUserRequest)
+	err := json.NewDecoder(r.Body).Decode(ur)
+	if err != nil {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		return
+	}
+	if ur.Email == "" || ur.Name == "" || ur.Password == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	us := models.NewUserService()
+	err = us.Create(&models.User{ID: "1", Name: ur.Name, Email: ur.Email, Password: ur.Password})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	} else {
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(ur)
+	}
+}
+
 func getUserHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	vars := mux.Vars(r)
@@ -35,6 +58,7 @@ func getUserHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	loadEnvVars()
 	r := mux.NewRouter()
+	r.HandleFunc("/users", createUserHandler).Methods("POST")
 	r.HandleFunc("/users/{user}", getUserHandler).Methods("GET")
 	fmt.Printf("Starting the server on :%d...\n", port)
 	http.ListenAndServe(fmt.Sprintf(":%d", port), r)
@@ -45,4 +69,10 @@ func loadEnvVars() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+}
+
+type createUserRequest struct {
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+	Password string `json:"password"`
 }
