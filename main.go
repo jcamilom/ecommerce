fbea53710e2036ae85364c11ac7c17cc20ec1a2e
Camilo Muñoz <jcamilomunozz@gmail.com>
2019-10-04 23:37:51 -0500

    Querying users from DynamoDB

--- main.go
+++ main.go
@@ -14,18 +14,21 @@ type User struct {
 	Password string `json:"password"`
 }
 
-var users = []User{
-	{ID: "1", Name: "juan", Email: "juan@mail.com", Password: "1234"},
-	{ID: "2", Name: "pedro", Email: "pedro@mail.com", Password: "1234"},
-}
-
-func getUsersHandler(w http.ResponseWriter, r *http.Request) {
+func getUserHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(users)
+	vars := mux.Vars(r)
+	usr, err := getUser(vars["user"])
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	} else if usr != nil {
+		json.NewEncoder(w).Encode(usr)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/users", getUsersHandler).Methods("GET")
+	r.HandleFunc("/users/{user}", getUserHandler).Methods("GET")
 	http.ListenAndServe(":3000", r)
 }
